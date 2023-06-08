3cd878c7fe77cca13b6295899dd75640c7491a9f
HarvelsX <90945793+HarvelsX@users.noreply.github.com>
2023-06-08 19:02:25 +0300

    Add scheduler adapter & migrate to use it;

--- src/com/pretzel/dev/villagertradelimiter/data/PlayerData.java
+++ src/com/pretzel/dev/villagertradelimiter/data/PlayerData.java
@@ -3,18 +3,20 @@ package com.pretzel.dev.villagertradelimiter.data;
 import com.pretzel.dev.villagertradelimiter.wrappers.VillagerWrapper;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PlayerData {
-    private final HashMap<String, String> tradingCooldowns;
+    private final Map<String, String> tradingCooldowns;
     private VillagerWrapper tradingVillager;
 
     public PlayerData() {
-        this.tradingCooldowns = new HashMap<>();
+        this.tradingCooldowns = new ConcurrentHashMap<>();
         this.tradingVillager = null;
     }
 
     /** @return The map of items to timestamps for the player's trading history */
-    public HashMap<String, String> getTradingCooldowns() { return this.tradingCooldowns; }
+    public Map<String, String> getTradingCooldowns() { return this.tradingCooldowns; }
 
     /** @param tradingVillager The villager that this player is currently trading with */
     public void setTradingVillager(final VillagerWrapper tradingVillager) { this.tradingVillager = tradingVillager; }
